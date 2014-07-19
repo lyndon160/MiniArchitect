f1c2d8183a60fa923a5952c9a81c584d2ffbc8b5
Lyndon Fawcett <lyndonfawcett@gmail.com>
2014-07-19 12:09:30 +0100

    First commit. Testing grade + git

---
+++ MiniArchitect-desktop/src/com/LyndonFawcett/MiniArchitect/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.LyndonFawcett.MiniArchitect.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.LyndonFawcett.MiniArchitect.MiniArchitect;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MiniArchitect(), config);
+	}
+}
